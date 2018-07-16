8a431774ac2933c676676e93d68073eabfdf2e78
Madeline Celis <madelinebcelis@gmail.com>
2018-07-16 17:39:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,9 @@ function updateObjectWithKeyAndValue(obj, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
+};
+
+function deleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
 }
